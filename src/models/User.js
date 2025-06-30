e14b27099f4eac9fd2a8ee21d92cbf523be8fc13
ivko <ivkovicd69@gmail.com>
2025-06-30 18:19:16 +0200

    stripe i mongodb2

--- src/models/User.js
+++ src/models/User.js
@@ -1,19 +1,11 @@
+// 📁 models/User.js
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  isPro: {
-    type: Boolean,
-    default: false
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+  email: { type: String, required: true, unique: true },
+  isPro: { type: Boolean, default: false }
 });
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+export default User;
+
